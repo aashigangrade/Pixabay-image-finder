55c2292745c126da908485dc64eac7b9af089678
Aashi <aashigangrade06@gmail.com>
2020-09-27 14:21:50 +0530

    Updates

---
+++ precache-manifest.a3c46f2de967e9ba0cf188c4da388481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3f1a17098e615a6ef9dc006d7e6da8",
+    "url": "/Pixabay-image-finder/index.html"
+  },
+  {
+    "revision": "3b1e709a83e525d1c735",
+    "url": "/Pixabay-image-finder/static/css/main.aaa47732.chunk.css"
+  },
+  {
+    "revision": "f79e952d6b5d727dd6dc",
+    "url": "/Pixabay-image-finder/static/js/2.8d2a7b58.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/Pixabay-image-finder/static/js/2.8d2a7b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1e709a83e525d1c735",
+    "url": "/Pixabay-image-finder/static/js/main.06eb1c93.chunk.js"
+  },
+  {
+    "revision": "e69459d297f1c7c04ac8",
+    "url": "/Pixabay-image-finder/static/js/runtime-main.b56a8a08.js"
+  }
+]);
